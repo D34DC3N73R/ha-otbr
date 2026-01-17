02768766196d2ef37acd82e200e19dccf870983e
D34DC3N73R <levi.wilcox@gmail.com>
2026-01-16 23:40:55 -0800

    clean up logs

--- rootfs/etc/s6-overlay/scripts/banner.sh
+++ rootfs/etc/s6-overlay/scripts/banner.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 # ==============================================================================
-# Home Assistant Community Add-on: Base Images
-# Displays a simple add-on banner on startup
+# OpenThread Border Router - Standalone Container Banner
 # ==============================================================================
 . /etc/s6-overlay/scripts/hassio_compat.sh
-if supervisor_ping; then
-    log_info '-----------------------------------------------------------'
-    log_info " Add-on: $(addon_name)"
-    log_info " $(addon_description)"
-    log_info '-----------------------------------------------------------'
 
-    log_info " Add-on version: $(addon_version)"
-    if var_true "$(addon_update_available)"; then
-        log_warn ' There is an update available for this add-on!'
-        log_warn " Latest add-on version: $(addon_version_latest)"
-        log_warn ' Please consider upgrading as soon as possible.'
-    else
-        log_info ' You are running the latest version of this add-on.'
-    fi
-
-    log_info " System: $(uname -s) ($(uname -m))"
-    log_info " Home Assistant Core: ${HOMEASSISTANT_INFO:-unknown}"
-    log_info " Home Assistant Supervisor: ${HASS_SUPERVISOR_INFO:-unknown}"
-
-    log_info '-----------------------------------------------------------'
-    log_info ' Please, share the above information when looking for help'
-    log_info ' or support in, e.g., GitHub, forums or the Discord chat.'
-    log_info '-----------------------------------------------------------'
-fi
+log_info '-----------------------------------------------------------'
+log_info " OpenThread Border Router (OTBR)"
+log_info " Standalone container - no supervisor required"
+log_info '-----------------------------------------------------------'
+log_info " Container: $(addon_hostname)"
+log_info " System: $(uname -s) ($(uname -m))"
+log_info " Backbone Interface: ${BACKBONE_IF:-auto-detected}"
+log_info " Device: ${DEVICE:-/dev/ttyACM0}"
+log_info '-----------------------------------------------------------'
+log_info ' GitHub: https://github.com/D34DC3N73R/ha-otbr'
+log_info '-----------------------------------------------------------'
